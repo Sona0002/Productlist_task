31ea99d41d67fa957d502abb98a806cdd93d5e62
Sona0002 <140248044+Sona0002@users.noreply.github.com>
2026-01-02 18:19:43 +0530

    Update list.js

--- frontend/src/components/list.js
+++ frontend/src/components/list.js
@@ -1,32 +1,52 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';  
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+function List() {
 
+  const [data, setData] = useState([]);
 
-function listproduct(){
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []); 
 
+  return (
+    <>
+      <h2>Products</h2>
 
-    const [products,setProducts] = useState([])
-
-    useEffect(()=>
-
-        try{
-
-            fetch('http://127.0.0.1:8000/')
-            response.json()
-
-        }
-
-
-
-        
-
-
-    )
-
-
-
+      <table className="table table-success table-striped">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+              <td>{item.description}</td>
+              <td>
+                <button className="btn btn-success">Update</button>&nbsp;
+                <button className="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default listproduct;
+export default List;
